0fb4bb2b5ec66d7562a301d56b2508f33eb139a2
katesamuel <csamuel.bi@gmail.com>
2024-01-20 22:25:35 +0000

    home page

--- webapp/src/app/modules/home/home.component.ts
+++ webapp/src/app/modules/home/home.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public images: string[] = [2,1,3,4,5,2,3,4,1,5,4,3,5,1,2,3,4,5,2,3,1,4,1,2,3,4,5,2,3,4,1,5,4,3,5,1,2,3,4,5,2,3,1,4].map(x => `../../../assets/images/home/image-marquee/notes-${x}.png`);
+  public year: number = new Date().getFullYear();
+  public academicYear: string = this.year + '-' + (this.year + 1);
+}
